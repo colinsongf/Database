4cfa027d13e3dad1136a87ac666b7dd2bf0b8cde
ncc1701k <tyranitr@gmail.com>
2015-12-20 17:58:05 -0500

    xefg calc

--- Scraping/py/generateshots.py
+++ Scraping/py/generateshots.py
@@ -7,6 +7,7 @@ import pandas as pd
 import tables
 import sys
 import csv
+import gc
 
 DATAPATH = '/media/sf_GitHub/Database/Modeling/refactored/data'
 
@@ -24,19 +25,21 @@ def write_global_xefg_by_date(datenum,season):
 
   #generate XeFG% by area 
   #get all shot attempts by area from game 1 to game x
-  
+  try:
 
-  fgm_by_area = df_shots_game.SHOT_MADE_FLAG.groupby(df_shots_game.SHOT_ZONE_BASIC).sum()
-  fga_by_area = df_shots_game.SHOT_ATTEMPTED_FLAG.groupby(df_shots_game.SHOT_ZONE_BASIC).sum()
+    fgm_by_area = df_shots_game.SHOT_MADE_FLAG.groupby(df_shots_game.SHOT_ZONE_BASIC).sum()
+    fga_by_area = df_shots_game.SHOT_ATTEMPTED_FLAG.groupby(df_shots_game.SHOT_ZONE_BASIC).sum()
 
-  atb3_pps = (fgm_by_area['Above the Break 3']*3+fga_by_area['Above the Break 3']*0.0125*3*0.8)/fga_by_area['Above the Break 3']
-  c3_pps = ((fgm_by_area['Left Corner 3']+fgm_by_area['Right Corner 3'])*3+(fga_by_area['Left Corner 3']+fga_by_area['Right Corner 3'])*0.01*3*0.8)/(fga_by_area['Right Corner 3']+fga_by_area['Left Corner 3'])
-  midrange_pps = (fgm_by_area['Mid-Range']*2+fga_by_area['Mid-Range']*0.052*2*0.8)/fga_by_area['Mid-Range']
-  ra_pps = (fgm_by_area['Restricted Area']*2+fga_by_area['Restricted Area']*0.22*2*0.8)/fga_by_area['Restricted Area']
-  paint_pps = (fgm_by_area['In The Paint (Non-RA)']*2+fga_by_area['In The Paint (Non-RA)']*0.1855*2*0.8)/fga_by_area['In The Paint (Non-RA)']
-  
-  writer.writerow([datelimit,atb3_pps,c3_pps,midrange_pps,ra_pps,paint_pps])
-  print(datelimit)
+    atb3_pps = (fgm_by_area['Above the Break 3']*3+fga_by_area['Above the Break 3']*0.0125*3*0.8)/fga_by_area['Above the Break 3']
+    c3_pps = ((fgm_by_area['Left Corner 3']+fgm_by_area['Right Corner 3'])*3+(fga_by_area['Left Corner 3']+fga_by_area['Right Corner 3'])*0.01*3*0.8)/(fga_by_area['Right Corner 3']+fga_by_area['Left Corner 3'])
+    midrange_pps = (fgm_by_area['Mid-Range']*2+fga_by_area['Mid-Range']*0.052*2*0.8)/fga_by_area['Mid-Range']
+    ra_pps = (fgm_by_area['Restricted Area']*2+fga_by_area['Restricted Area']*0.22*2*0.8)/fga_by_area['Restricted Area']
+    paint_pps = (fgm_by_area['In The Paint (Non-RA)']*2+fga_by_area['In The Paint (Non-RA)']*0.1855*2*0.8)/fga_by_area['In The Paint (Non-RA)']
+    
+    writer.writerow([datelimit,atb3_pps,c3_pps,midrange_pps,ra_pps,paint_pps])
+    gc.collect()
+  except KeyError:
+    print ("first day of season, lol")
 
 
 #get all shot made by area from game 1 to game x
@@ -50,6 +53,7 @@ def read_gamelist(datelist):
     datenum = r.split(',')[0]
     seasonfull = r.split(',')[1]
     season = seasonfull[2:4]
+    print(datenum)
     write_global_xefg_by_date(datenum,season)
 
 def main():
